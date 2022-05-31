b826a44d90727eb0c2547cd6b65948cb573f1296
Maksim Pinigin <at@pinig.in>
2022-05-31 09:19:12 +0700

    2022 update

--- delays.go
+++ delays.go
@@ -3,9 +3,9 @@ package cupfmc
 import "time"
 
 var (
-  Dot = 60 * time.Millisecond
-  Delay = Dot
-  Sign = Dot * 3
-  Word = Dot * 7
-  Dash = Dot * 3
+	Dot   = 60 * time.Millisecond
+	Delay = Dot
+	Sign  = Dot * 3
+	Word  = Dot * 7
+	Dash  = Dot * 3
 )
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/estintax/cupfmc
+
+go 1.18
